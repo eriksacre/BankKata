6c60703459e8ee77ac0bfc129702eea6e548bd8b
Erik Sacre <erik.sacre@gmail.com>
2018-08-17 15:42:24 +0200

    Setup the project for testing. Create a do-nothing test.

---
+++ BankKata.Tests/Features/PrintStatement.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace BankKata.Tests.Features
+{
+    public class PrintStatement
+    {
+        [Test]
+        public void Nothing()
+        {
+            Assert.That(false, Is.False);
+        }
+    }
+}
