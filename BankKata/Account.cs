b772dd007ac122efb7c292afd98d745288d4bc18
Erik Sacre <erik.sacre@gmail.com>
2018-08-17 15:46:53 +0200

    Add skeleton for Account class, as defined in the README.

---
+++ BankKata/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankKata
+{
+    public class Account
+    {
+        public void Deposit(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdrawal(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintStatement()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
