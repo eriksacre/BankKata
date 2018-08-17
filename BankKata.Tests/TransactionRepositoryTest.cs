e92d8ae62073d679c7ddc54f83328006489f217c
Erik Sacre <erik.sacre@gmail.com>
2018-08-17 18:01:43 +0200

    TransactionRepositoryTest All_MultipleTransactionsInRepo_ReturnsTransactionsInInsertionOrder completed

--- BankKata.Tests/TransactionRepositoryTest.cs
+++ BankKata.Tests/TransactionRepositoryTest.cs
@@ -15,5 +15,21 @@ namespace BankKata.Tests
             Assert.That(transactionRepository.All().Count, Is.EqualTo(1));
             Assert.That(transactionRepository.All()[0], Is.EqualTo(transaction));
         }
+
+        [Test]
+        public void All_MultipleTransactionsInRepo_ReturnsTransactionsInInsertionOrder()
+        {
+            var transactionRepository = new TransactionRepository();
+            var transaction1 = new Transaction("01/01/2018", 100);
+            var transaction2 = new Transaction("02/01/2018", -50);
+            transactionRepository.Add(transaction1);
+            transactionRepository.Add(transaction2);
+
+            var transactions = transactionRepository.All();
+            
+            Assert.That(transactions.Count, Is.EqualTo(2));
+            Assert.That(transactions[0], Is.EqualTo(transaction1));
+            Assert.That(transactions[1], Is.EqualTo(transaction2));
+        }
     }
 }
