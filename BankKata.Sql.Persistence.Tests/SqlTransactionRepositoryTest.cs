9f9ff285452d83e0d69d0ee71e8ad116db00fd6f
Erik Sacre <erik.sacre@gmail.com>
2018-08-24 15:58:48 +0200

    Add Sql persistence test project.

---
+++ BankKata.Sql.Persistence.Tests/SqlTransactionRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using BankKata.Domain;
+using BankKata.Tests;
+using NUnit.Framework;
+
+namespace BankKata.Sql.Persistence.Tests
+{
+    [TestFixture]
+    public class SqlTransactionRepositoryTest : TransactionRepositoryBaseTest
+    {
+        protected override ITransactionRepository NewRepository()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
