448a8a7fef18009400da2cc77eeedc2d5cb379b2
Erik Sacre <erik.sacre@gmail.com>
2018-08-25 19:07:29 +0200

    Extract DatabaseCleanerBase to separate reusable bits from non-reusable ones

--- BankKata.Sql.Persistence.Tests/DatabaseCleaner.cs
+++ BankKata.Sql.Persistence.Tests/DatabaseCleaner.cs
@@ -1,46 +1,17 @@
 ﻿using System.Collections.Generic;
-using System.Data.SqlClient;
 
 namespace BankKata.Sql.Persistence.Tests
 {
-    public class DatabaseCleaner
+    public class DatabaseCleaner : DatabaseCleanerBase
     {
-        private readonly string _connectionString;
+        public DatabaseCleaner(string connectionString) : base(connectionString) {}
 
         // Note:
         // Order is important. First truncate the tables that reference
         // other tables.
-        private static readonly List<string> TablesToClear = new List<string>
+        protected override List<string> TablesToClear => new List<string>
         {
             "Transactions"
         };
-
-        public DatabaseCleaner(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-        
-        public void Clean()
-        {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-                TruncateTables(connection);
-            }
-        }
-
-        private void TruncateTables(SqlConnection connection)
-        {
-            foreach (var table in TablesToClear)
-            {
-                Truncate(table, connection);
-            }
-        }
-
-        private void Truncate(string table, SqlConnection connection)
-        {
-            var truncate = new SqlCommand($"truncate table {table}", connection);
-            truncate.ExecuteNonQuery();
-        }
     }
 }
---
+++ BankKata.Sql.Persistence.Tests/DatabaseCleanerBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace BankKata.Sql.Persistence.Tests
+{
+    public abstract class DatabaseCleanerBase
+    {
+        private readonly string _connectionString;
+
+        protected DatabaseCleanerBase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public void Clean()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                TruncateTables(connection);
+            }
+        }
+        
+        protected abstract List<string> TablesToClear { get; }
+
+        private void TruncateTables(SqlConnection connection)
+        {
+            foreach (var table in TablesToClear)
+            {
+                Truncate(table, connection);
+            }
+        }
+
+        private void Truncate(string table, SqlConnection connection)
+        {
+            var truncate = new SqlCommand($"truncate table {table}", connection);
+            truncate.ExecuteNonQuery();
+        }
+    }
+}
