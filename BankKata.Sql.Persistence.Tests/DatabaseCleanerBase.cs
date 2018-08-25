68921eec6c9277c47615892ea98769cfb09e7f88
Erik Sacre <erik.sacre@gmail.com>
2018-08-25 21:03:59 +0200

    Add DatabaseCleanerTest to ensure all tables are either truncated or in a do-not-truncate list

--- BankKata.Sql.Persistence.Tests/DatabaseCleanerBase.cs
+++ BankKata.Sql.Persistence.Tests/DatabaseCleanerBase.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace BankKata.Sql.Persistence.Tests
 {
     public abstract class DatabaseCleanerBase
     {
         private readonly string _connectionString;
+
+        private static readonly List<string> _doNotTruncate = new List<string>
+        {
+            "SchemaVersions"
+        };
 
         protected DatabaseCleanerBase(string connectionString)
         {
@@ -20,7 +26,37 @@ namespace BankKata.Sql.Persistence.Tests
                 TruncateTables(connection);
             }
         }
-        
+
+        public string MissingTables()
+        {
+            var tables = GetTableNames();
+            var tablesToClear = TablesToClear;
+            var missingTables = tables
+                .Where(tableName => !tablesToClear.Contains(tableName) && !_doNotTruncate.Contains(tableName));
+
+            return string.Join(",", missingTables);
+        }
+
+        private List<string> GetTableNames()
+        {
+            var tables = new List<string>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_TYPE='BASE TABLE'",
+                    connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        tables.Add(reader["TABLE_NAME"].ToString());
+                    }
+                }
+            }
+
+            return tables;
+        }
+
         protected abstract List<string> TablesToClear { get; }
 
         private void TruncateTables(SqlConnection connection)
---
+++ BankKata.Sql.Persistence.Tests/DatabaseCleanerTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace BankKata.Sql.Persistence.Tests
+{
+    public class DatabaseCleanerTest
+    {
+        private const string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=BankKata;Trusted_Connection=True";
+
+        [Test]
+        public void VerifyAllTablesAreIncluded()
+        {
+            var cleaner = new DatabaseCleaner(ConnectionString);
+            
+            var missingTables = cleaner.MissingTables();
+            
+            Assert.That(missingTables, Is.EqualTo(""));
+        }
+    }
+}
